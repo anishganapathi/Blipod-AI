2e8908ad413e98fa43cd5f2bb53f9bd519026fa1
Anish <104456169+anishganapathi@users.noreply.github.com>
2025-09-22 02:17:04 -0400

    + Added InterestsPage

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import GlassNavBar from '@/components/GlassNavBar';
 import Login from './auth/login';
+import InterestsPage from './interests';
+
 export default function Index() {
-  return <GlassNavBar />;
+  return <InterestsPage />;
 }
