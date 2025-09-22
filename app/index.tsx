861ddc726a90f4a02756d4c8d3435385fd521fc6
Anish <104456169+anishganapathi@users.noreply.github.com>
2025-09-22 02:37:16 -0400

    + Added SplashPage

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import GlassNavBar from '@/components/GlassNavBar';
 import Login from './auth/login';
 import InterestsPage from './interests';
+import SplashScreen from '@/components/SplashPage';
 
 export default function Index() {
-  return <InterestsPage />;
+  return <GlassNavBar />;
 }
